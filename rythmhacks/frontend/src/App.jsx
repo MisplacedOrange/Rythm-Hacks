de42cd705e64e6e970fe269021e3fd1e702ad8d5
Crackle2K <crackleontwitch@gmail.com>
2025-10-25 14:13:38 -0400

    Discord Webhook Implementation

--- rythmhacks/frontend/src/App.jsx
+++ rythmhacks/frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './Home'
+import Contact from './Contact'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   )
